f5e2bb317b479f1dc647b8aa97522debd6791b30
2017Yasu <yasu.nkym.96@gmail.com>
2022-03-05 22:39:51 +0900

    Set my database in local_settings.py

--- Cycling/settings.py
+++ Cycling/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Cycling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES_TMP = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
